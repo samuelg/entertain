52ac3a41c57c7626c962b8babb800677d3b07997
Samuel Galarneau <samuelg@samuelg-laptop.(none)>
2010-02-12 22:54:43 -0600

    Added elastic css framework.
    Added development only serving of static files.

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
 from weekly.views import default
+import settings
 
 admin.autodiscover()
 
@@ -9,3 +10,12 @@ urlpatterns = patterns('',
     (r'^weekly/', include('weekly.urls')),
     (r'^$', default),
 )
+
+# setup to server static files in development mode
+if settings.DEBUG:
+    if settings.MEDIA_URL.startswith("/"):
+        media_url = settings.MEDIA_URL[1:]
+        urlpatterns += patterns('',
+            (r'^%s(?P<path>.*)$' % media_url, 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+        )
+
